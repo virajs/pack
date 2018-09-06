95aa18340c7a933bc1838d2e011c36a1026f4dab
Dave Goddard <dave@goddard.id.au>
2018-09-06 19:48:54 -0300

    attempt to generate image from tar files

--- exporter.go
+++ exporter.go
@@ -1,10 +1,18 @@
 package pack
 
 import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net"
+	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/buildpack/lifecycle"
@@ -72,6 +80,123 @@ func export(group lifecycle.BuildpackGroup, launchDir, repoName, stackName strin
 }
 
 func simpleExport(group lifecycle.BuildpackGroup, launchDir, repoName, stackName string) (string, error) {
+	// metadata := packs.BuildMetadata{
+	// 	App:        packs.AppMetadata{},
+	// 	Buildpacks: []packs.BuildpackMetadata{},
+	// 	RunImage: packs.RunImageMetadata{
+	// 		SHA: stackDigest.String(),
+	// 	},
+	// }
+
+	httpc := http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", "/var/run/docker.sock")
+			},
+		},
+	}
+
+	res, err := httpc.Get("http://unix/images/" + stackName + "/get")
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return "", fmt.Errorf("expected 200: actual: %d", res.StatusCode)
+	}
+
+	r, w := io.Pipe()
+	tarball := tar.NewWriter(w)
+
+	go func() {
+		tarReader := tar.NewReader(res.Body)
+		var parentLayerID string
+		for {
+			header, err := tarReader.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				panic(err)
+			}
+
+			if !(strings.HasSuffix(header.Name, "/VERSION") || strings.HasSuffix(header.Name, "/json") || strings.HasSuffix(header.Name, "/layer.tar")) {
+				continue
+			}
+
+			// fmt.Println(header.Name, header.FileInfo())
+
+			if err := tarball.WriteHeader(header); err != nil {
+				panic(err)
+			}
+			if _, err = io.Copy(tarball, tarReader); err != nil {
+				panic(err)
+			}
+
+			parentLayerID = strings.Split(header.Name, "/")[0]
+		}
+
+		layerDirs := []string{"/launch/app", "/launch/config"}
+		for _, buildpack := range group.Buildpacks {
+			dirs, err := filepath.Glob(filepath.Join(launchDir, buildpack.ID, "*.toml"))
+			if err != nil {
+				panic(err)
+			}
+			for _, dir := range dirs {
+				dir = dir[:len(dir)-5]
+				fmt.Println("DIR:", dir)
+				if _, err := os.Stat(dir); os.IsNotExist(err) {
+					fmt.Println("DIR NOT EXIST:", dir)
+					continue
+				} else if err != nil {
+					panic(err)
+				}
+				dir, err = filepath.Rel(launchDir, dir)
+				if err != nil {
+					panic(err)
+				}
+				layerDirs = append(layerDirs, dir)
+			}
+		}
+
+		fmt.Println("LAYER DIRS:", layerDirs)
+
+		for _, name := range layerDirs {
+			b, err := tarDir(filepath.Join(launchDir, name), "launch/"+name)
+			if err != nil {
+				panic(err)
+			}
+			layerID := fmt.Sprintf("%x", sha256.Sum256(b))
+			addFileToTar(tarball, layerID+"/VERSION", []byte("1.0"))
+			addFileToTar(tarball, layerID+"/json", []byte(fmt.Sprintf(`{"id":"%s","parent":"%s","os":"linux"}`, layerID, parentLayerID)))
+			addFileToTar(tarball, layerID+"/layer.tar", b)
+			parentLayerID = layerID
+		}
+
+		// TODO repoName may have a tag, need to fix that
+		if err := addFileToTar(tarball, "repositories", []byte(fmt.Sprintf(`{"%s":{"latest":"%s"}}`, repoName, parentLayerID))); err != nil {
+			panic(err)
+		}
+
+		tarball.Close()
+		w.Close()
+	}()
+
+	res, err = httpc.Post("http://unix/images/load", "application/tar", r)
+	r.Close()
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return "", fmt.Errorf("expected 200: actual: %d", res.StatusCode)
+	}
+	fmt.Printf("POST: %#v\n", res)
+	io.Copy(os.Stdout, res.Body)
+
+	return "TODO", nil
+}
+
+func simpleExportOld(group lifecycle.BuildpackGroup, launchDir, repoName, stackName string) (string, error) {
 	cmd := exec.Command("docker", "build", "--force-rm", "-t", repoName, "-f", "-", launchDir)
 	cmd.Dir = launchDir
 	cmd.Stdin = strings.NewReader(fmt.Sprintf("FROM %s\nCOPY . /launch\n", stackName))
@@ -80,4 +205,54 @@ func simpleExport(group lifecycle.BuildpackGroup, launchDir, repoName, stackName
 		return "", err
 	}
 	return "TODO", nil
+}
+
+func addFileToTar(w *tar.Writer, path string, contents []byte) error {
+	if err := w.WriteHeader(&tar.Header{Name: path, Size: int64(len(contents))}); err != nil {
+		return err
+	}
+	_, err := w.Write([]byte(contents))
+	return err
+}
+
+func tarDir(srcDir, destDir string) ([]byte, error) {
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		destPath, err := filepath.Rel(srcDir, path)
+		if err != nil {
+			return err
+		}
+		destPath = filepath.Join(destDir, destPath)
+		if info.IsDir() {
+			return nil
+		}
+		if info.Mode()&os.ModeSymlink != 0 {
+			fmt.Println("SYMLINK:", destPath)
+			// TODO handle correctly
+			return nil
+		}
+
+		if err := tw.WriteHeader(&tar.Header{
+			Name:    destPath,
+			Size:    info.Size(),
+			Mode:    int64(info.Mode()),
+			ModTime: info.ModTime(),
+		}); err != nil {
+			return err
+		}
+
+		fh, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer fh.Close()
+		_, err = io.Copy(tw, fh)
+		return err
+	})
+	tw.Close()
+	return buf.Bytes(), err
 }
